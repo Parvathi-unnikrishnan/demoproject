96882d657bc3013d030336b5d2f2b5344ec03d86
Parvathi-unnikrishnan <parvathi.unnikrishnan0@gmail.com>
2024-11-01 06:53:06 -0700

    added1

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from shop.models import Category, Product
-
+from django import HttpResponse
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
